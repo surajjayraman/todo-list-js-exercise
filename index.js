441d70d21bab89f1be9a47d3acde5d77a545281e
surajjayraman <suraj.jayraman@gmail.com>
2021-08-27 19:48:25 -0400

    refactored Todo List using OO Principles

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+/*function newTaskOld(title) {
   taskTitles.push(title);
   taskComplete.push(false);
+}*/
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
+/*function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
 }
 
@@ -23,9 +39,41 @@ function logTaskState(taskIndex) {
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+//newTaskOld("Clean Cat Litter"); // task 0
+//newTaskOld("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
+
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+
+/*const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);*/
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
